025c7c088339e417f2524db08245832616726b2a
Ernest <ernesttancw@gmail.com>
2022-03-21 11:11:17 -0400

    Initial Setup

---
+++ app.js
@@ -0,0 +1,30 @@
+// function Book (title, author, pages, read) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read
+//     this.info = function() {
+//         return `${title} by ${author}, ${pages} pages, ${read}`;
+//     }
+// }
+
+// const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, 'not read yet');
+// console.log(theHobbit.info());
+
+// Books will be stored here
+let myLibrary = []
+
+// Book Constructor
+function Book() {
+
+}
+
+// Function to add book to library
+function addBookToLibrary() {
+
+}
+
+// Function to display all books
+function displayBooks(library) {
+
+}
